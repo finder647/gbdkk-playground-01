c71a8d8ef692012f2f24fc50bafcbb498e76885f
finder647 <arcahyadiindra@gmail.com>
2020-07-01 07:14:28 +0700

    Minor update to `InputSystemTest.cs` script. Test out some ways to move character
    * Also change project's Physics2D gravity to -29.81f for more juicy jump

--- Assets/Scripts/Test/InputSystemTest.cs
+++ Assets/Scripts/Test/InputSystemTest.cs
@@ -7,15 +7,22 @@ namespace GBDKK.Test
     public class InputSystemTest : MonoBehaviour
     {
         [SerializeField]
-        private float _speed = 10;
+        private float _speed = 15;
         [SerializeField]
-        private float _jumpPower = 30;
+        private float maxVelocity = 20;
+        [SerializeField]
+        private float _movementSmoothing = .5f;
+        [SerializeField]
+        private float _jumpPower = 300;
+        [SerializeField]
+        private float _jumpHeight = 7;
         [SerializeField]
         private Rigidbody2D _rb2D;
 
         private TestControl _inputActionMap;
         private float _horMoveAxis;
         private bool _shouldJump;
+        private Vector2 _refVelocity;
 
         private void OnEnable()
         {
@@ -48,19 +55,28 @@ namespace GBDKK.Test
         private void HandleJump(InputAction.CallbackContext context)
         {
             _shouldJump = true;
+            //_rb2D.AddForce(Vector2.up * Mathf.Sqrt(_jumpHeight * -2f * Physics2D.gravity.y), ForceMode2D.Impulse);
             Debug.Log("Jump Called");
         }
 
         private void FixedUpdate()
         {
+            //if (Mathf.Abs(_rb2D.velocity.magnitude) < maxVelocity)
+            //{
+            //    _rb2D.AddForce(new Vector2(_horMoveAxis * _speed, 0));
+            //}
+
+            Vector2 targetVelocity = new Vector2(_horMoveAxis * _speed, 0);
+            _rb2D.velocity =
+                Vector2.SmoothDamp(_rb2D.velocity, targetVelocity, ref _refVelocity, _movementSmoothing);
+
             if (_shouldJump)
             {
                 _rb2D.AddForce(Vector2.up * _jumpPower);
                 _shouldJump = false;
             }
 
-            _rb2D.AddForce(new Vector2(_horMoveAxis * _speed, 0));
-            //_rb2D.MovePosition(_rb2D.position + new Vector2(_horMoveAxis * Time.deltaTime * _speed, 0));
+            //_rb2D.MovePosition(_rb2D.position + (Vector2.right * _horMoveAxis * _speed * Time.fixedDeltaTime));
         }
     }
 }
